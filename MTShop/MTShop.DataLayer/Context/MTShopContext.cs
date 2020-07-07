4efc76c339b61ec57fee0574c2aa3489827d839c
علی <@DESKTOP-HTFBVBU>
2020-07-07 16:31:55 +0430

    add MTShopContext

---
+++ MTShop/MTShop.DataLayer/Context/MTShopContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MTShop.DataLayer.Context
+{
+   public class MTShopContext:DbContext
+   {
+
+       public MTShopContext(DbContextOptions<MTShopContext> options):base(options)
+        {
+            
+        }
+
+        #region User
+
+
+
+        #endregion
+
+        #region Product
+
+        
+
+        #endregion
+
+
+    }
+}
