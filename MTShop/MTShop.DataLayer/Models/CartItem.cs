c55b5da5058077484f76e6f10c52e8f55cd23dd5
Reza <Reza@DESKTOP-2OVE4Q4>
2020-07-07 16:19:03 +0430

    Models

---
+++ MTShop/MTShop.DataLayer/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTShop.DataLayer.Models
+{
+   public class CartItem
+    {
+        public int Id { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+        public decimal getTotalPrice()
+        {
+            return Item.Price = Quantity;
+        }
+
+
+    }
+}
---
+++ MTShop/MTShop.DataLayer/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTShop.DataLayer.Models
+{
+  public  class Item
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int QuantityInStock { get; set; }
+    }
+}
