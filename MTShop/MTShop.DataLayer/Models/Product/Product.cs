21c720919c4556055836336782c9dea1b41bd98c
علی <@DESKTOP-HTFBVBU>
2020-07-10 04:02:31 +0430

    اصلاح مدل محصول

--- MTShop/MTShop.DataLayer/Models/Product/Product.cs
+++ MTShop/MTShop.DataLayer/Models/Product/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MTShop.DataLayer.Models.Product
@@ -10,12 +11,29 @@ namespace MTShop.DataLayer.Models.Product
         {
             Categories = new List<Category>();
         }
-
+        [Key]
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        [Display(Name = "نام محصول")]
+        [Required(ErrorMessage = "لطفا نام محصول را وارد کنید.")]
+        [MaxLength(100, ErrorMessage = "نام محصول نمی تواند بیشتر از 100 کاراکتر باشد.")]
+        public string ProductName { get; set; }
 
+        [Display(Name = "توضیحات")]
         public string Description { get; set; }
+
+        [MaxLength(50)]
+        public string ImageName { get; set; }
+
+        [Display(Name = "حذف شده؟")]
+        public bool IsDelete { get; set; }
+
+        [Display(Name = "موجود است؟")]
+        public bool IsExist { get; set; }
+
+        [Display(Name = "قیمت")]
+        [Required(ErrorMessage = "لطفا قیمت محصول را وارد کنید.")]
+        public decimal Price { get; set; }
 
         #region Relations
         public List<Category> Categories { get; set; }
