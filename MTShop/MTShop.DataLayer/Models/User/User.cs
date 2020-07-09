092bfc128ea1bb49967efa5489c628b4b3f1b9a2
علی <@DESKTOP-HTFBVBU>
2020-07-09 15:50:35 +0430

    اصلاح مدل کاربر

--- MTShop/MTShop.DataLayer/Models/User/User.cs
+++ MTShop/MTShop.DataLayer/Models/User/User.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace MTShop.DataLayer.Models.User
 {
-  
+
     public class User
     {
         public User()
@@ -16,16 +16,33 @@ namespace MTShop.DataLayer.Models.User
         [Key]
         public int UserId { get; set; }
 
-        [Required]
-        [MaxLength(300)]
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا نام کاربری را وارد کنید.")]
+        [MaxLength(100, ErrorMessage = "نام کاربری نمی تواند بیشتر از 100 کاراکتر باشد.")]
+        public string UserName { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا ایمیل خود را وارد کنید.")]
+        [MaxLength(100, ErrorMessage = "ایمیل نمی تواند بیشتر از 100 کاراکتر باشد.")]
         public string Email { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "لطفا رمز عبور را وارد کنید.")]
+        [MaxLength(50, ErrorMessage = "رمز عبور نمی تواند بیشتر از 50 کاراکتر باشد.")]
         public string Password { get; set; }
 
-        [Required]
+        [Display(Name = "تاریخ ثبت نام")]
         public DateTime RegisterDate { get; set; }
+
+        [Display(Name = "وضعیت")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "کد فعال سازی ")]
+        [MaxLength(50)]
+        public string ActivationCode { get; set; }
+
+        [Display(Name = "حذف شده؟")]
+        public bool IsDelete { get; set; }
 
 
         #region Relations
