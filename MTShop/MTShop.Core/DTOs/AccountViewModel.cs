6a17e22ba726ee8c89e73fcabb8afaa5a0795115
Reza <Reza@DESKTOP-2OVE4Q4>
2020-07-10 13:42:59 +0430

    ViewModel

--- MTShop/MTShop.Core/DTOs/AccountViewModel.cs
+++ MTShop/MTShop.Core/DTOs/AccountViewModel.cs
@@ -5,27 +5,71 @@ using System.Text;
 
 namespace MTShop.Core.DTOs
 {
-        public class RegisterViewModel
-        {
-            [MaxLength(300)]
-            [EmailAddress]
-            [Display(Name = "ایمیل")]
-            [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
-            public string Email { get; set; }
+    public class RegisterViewModel
+    {
+        [MaxLength(300)]
+        [EmailAddress]
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        public string Email { get; set; }
 
-            [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
-            [MaxLength(50)]
-            [DataType(DataType.Password)]
-            [Display(Name = "کلمه عبور")]
-            public string Password { get; set; }
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Display(Name = "کلمه عبور")]
+        public string Password { get; set; }
 
-            [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
-            [MaxLength(50)]
-            [DataType(DataType.Password)]
-            [Compare("Password")]
-            [Display(Name = "تکرار کلمه عبور")]
-            public string RePassword { get; set; }
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید ")]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [Display(Name = "تکرار کلمه عبور")]
+        public string RePassword { get; set; }
 
-        }
-    
+    }
+
+    public class LoginviewModel
+    {
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Password { get; set; }
+
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Display(Name = "")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        public string ActiveCode { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Password { get; set; }
+
+        [Display(Name = "تکرار کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [Compare("Password", ErrorMessage = "کلمه های عبور مغایرت دارند")]
+        public string RePassword { get; set; }
+
+    }
+
+
 }
