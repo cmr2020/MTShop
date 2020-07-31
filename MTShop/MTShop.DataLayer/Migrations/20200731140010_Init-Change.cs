ba7148951c02d188c22f122b829d40ea34471b92
Reza <Reza@DESKTOP-2OVE4Q4>
2020-07-31 18:34:01 +0430

    Init-DataBase

---
+++ MTShop/MTShop.DataLayer/Migrations/20200731140010_Init-Change.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MTShop.DataLayer.Migrations
+{
+    public partial class InitChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
